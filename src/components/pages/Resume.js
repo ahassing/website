9332e282d919ae1f5b61c1abf6757d4a4cf98047
Andrew Hassing <hassiar@huskers.unl.edu>
2022-01-06 21:32:49 -0600

    refactor and actually adding durties to the resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,20 +4,13 @@ import { work } from "../../data/work";
 const renderPositions = (position, poindex) => {
     return (
         <div className="item" key={`position${poindex}`}>
-            <div>
+            <div className="ui tiny header">
                 {position.title}
             </div>
             <div>
-                {position.startDate} - {position.endDate}
-            </div>
-            <div className="ui bulleted list">
-                {position.duties.map( (duty, duindex) =>{
-                    return (
-                        <div className="item" key={`duty${duindex}`}>
-                            {duty}
-                        </div>
-                    )
-                })}
+                <em>
+                    {position.startDate} - {position.endDate}
+                </em>
             </div>
         </div>
     )
@@ -29,11 +22,23 @@ const renderResume = (data) => {
             {data.map((company, coindex) => {
                 return (
                     <div className="item" key={`company${coindex}`} >
-                        <div className="header">
+                        <div className="ui header">
                             {company.name}
+                        </div>
+                        <div>
+                            {company.location}
                         </div>
                         <div className="ui list">
                             {company.positions.map((position, poindex) => renderPositions(position, poindex))}
+                        </div>
+                        <div className="ui bulleted list">
+                            {company.duties.map( (duty, duindex) =>{
+                                return (
+                                    <div className="item" key={`duty${duindex}`}>
+                                        {duty}
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                 );
--- src/data/work.js
+++ src/data/work.js
@@ -2,90 +2,103 @@ export const work = [
     {
         name: 'Spreetail, Inc.',
         website: '',
+        location: 'Omaha, NE',
+        duties: [
+            'Member of the Supply Chain - Operations software squad',
+            'test',
+        ],
         positions: [
             {
                 title: 'Software Engineer II',
                 startDate: 'May 2021',
-                endDate: 'Current',
-                location: 'Omaha, NE',
-                duties: [
-                    'Member of the Supply Chain - Operations software squad',
-                    'test',
-                ]
+                endDate: 'Current'
             }
         ],
     },
     {
         name: 'Northrop Grumman',
         website: '',
+        location: 'Bellevue, NE',
+        duties: [
+            'Support the NPES-RECAP project for U.S. Strategic Command.',
+            'Facilitate Agile ceremonies as a scrum master.',
+            'Develop a mapping prototype in using Angular and Leaflet libraries.',
+            'Support the SEMS III contract for helping U.S. Air Force Weather (AFW).',
+            'Develop weather data ingestion and visualization products for the AFW-WEBS Website using Visual Weather.',  
+            'Work with Open Geospatial Consortium (OGC) Standards for web services.',
+            'Replace Java distributed caching platform, reducing vulnerability findings.',
+            'Help develop full scale restructure of backend source code repositories.',
+            'Integrate first non-SEMS developed OGC products onto AFW-WEBS.',
+            'Help transition existing on-premises infrastructure into a cloud DI2E environment.',
+            'Provide on-call assistance.',
+            'Work in an Agile environment and provide back-up scrum master duties.'
+        ],
         positions: [
             {
                 title: 'Engineer Software',
                 startDate: 'May 2020',
-                endDate: 'April 2021',
-                location: 'Bellevue, NE',
-                duties: [
-                    'test',
-                    'test',
-                ]
+                endDate: 'April 2021'
             },
             {
                 title: 'Associate Engineer Software',
                 startDate: 'June 2017',
-                endDate: 'May 2020',
-                location: 'Bellevue, NE',
-                duties: [
-                    'test',
-                    'test',
-                ]
+                endDate: 'May 2020'
             }
         ],
     },
     {
         name: 'Tenaska, Inc.',
         website: '',
+        location: 'Omaha, NE',
+
+        duties: [
+            'Maintained and enhanced enterprise systems.',
+            'Developed build scripts for a web application.',
+            'Provided on-call support through error monitoring and help desk tickets.'
+        ],
         positions: [
             {
                 title: 'C#/.Net Development Intern',
                 startDate: 'January 2016',
-                endDate: 'May 2017',
-                location: 'Omaha, NE',
-                duties: [
-                    'Member of the Supply Chain - Operations squad',
-                    'test',
-                ]
+                endDate: 'May 2017'
             }
         ],
     },
     {
         name: 'Resource Insurance Consultants',
         website: '',
+        location: 'Omaha, NE',
+        duties: [
+            'Prepared LifeTrack reports to analyze insurance trusts for adequacy, competitiveness, carrier strength, and any other sources of concern.',
+            'Quality checked other analysts’ LifeTrack reports.',
+            'Ran preliminary analysis to find candidates for possible life settlements.',
+            'Modeled and analyzed financial feasibility for life settlements in SettleTrack report.'
+        ],
         positions: [
             {
                 title: 'Life Insruance Analyst',
                 startDate: 'August 2012',
-                endDate: 'January 2016',
-                location: 'Omaha, NE',
-                duties: [
-                    'Member of the Supply Chain - Operations squad',
-                    'test',
-                ]
+                endDate: 'January 2016'
             }
         ],
     },
     {
         name: 'Milliman, Inc.',
         website: '',
+        location: 'Omaha, NE',
+        duties: [
+            'Worked with clients in life insurance.',
+            'Prepared and ran cash flow testing model for annual analysis.',
+            'Maintained pricing models for term insurance, universal life, and final expense insurance.',
+            'Performed valuations on annuity blocks of business.',
+            'Upgraded models through prior versions of MG-ALFA.',
+            'Worked heavily in MG-ALFA and Excel.'
+        ],
         positions: [
             {
                 title: 'Actuarial Assistant',
                 startDate: 'October 2009',
-                endDate: 'May 2012',
-                location: 'Omaha, NE',
-                duties: [
-                    'Member of the Supply Chain - Operations squad',
-                    'test',
-                ]
+                endDate: 'May 2012'
             }
         ],
     },
