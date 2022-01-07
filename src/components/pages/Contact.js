a5ec5736cf99fe68e432aca5fc4fdb40ce9aefce
Andrew Hassing <hassiar@huskers.unl.edu>
2022-01-06 22:45:35 -0600

    Socials to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+const renderSocials = () => {
+    return (
+        <h3 className="ui icon">
+            <a href="https://www.facebook.com/hassingar"><i className='facebook icon'></i></a>
+            <a href="https://www.linkedin.com/in/ahassing/"><i className='linkedin icon'></i></a>
+            <a href="https://github.com/ahassing"><i className='github icon'></i></a>
+            <a href="mailto:andrewhassing@gmail.com"><i className='envelope icon'></i></a>
+        </h3>
+    )
+}
+
 const Contact = () => {
     return (
         <div>
             <h2>Contact</h2>
+            <p>Feel free to reach out to me for any reason. I'm friendly!</p>
+            <div className="ui divider"></div>
+            {renderSocials()}
         </div>
     );
 }
