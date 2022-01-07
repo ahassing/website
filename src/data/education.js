cf63c8a68ccb350c0bac788a6802026a4a7c7797
Andrew Hassing <hassiar@huskers.unl.edu>
2022-01-06 22:06:50 -0600

    formatting

--- src/data/education.js
+++ src/data/education.js
@@ -1,32 +1,32 @@
 export const education = [
-        {
-            name: 'University of Nebraska at Omaha',
-            location: 'Omaha, NE',
-            degrees: [
-                {
-                    degree: 'Masters of Science',
-                    major: 'Computer Science',
-                    graduationDate: 'May 2019',
-                    gpa: '3.6'
-                },
-                {
-                    degree: 'Bachelor of Science',
-                    major: 'Computer Science',
-                    graduationDate: 'May 2017',
-                    gpa: '3.8'
-                }
-            ]
-        },
-        {
-            name: 'University of Nebraska at Lincoln',
-            location: 'Lincoln, NE',
-            degrees: [
-                {
-                    degree: 'Bachelor of Science',
-                    major: 'Math and Actuary Science',
-                    graduationDate: 'May 2009',
-                    gpa: '3.8'
-                }
-            ]
-        },
-    ];
+    {
+        name: 'University of Nebraska at Omaha',
+        location: 'Omaha, NE',
+        degrees: [
+            {
+                degree: 'Masters of Science',
+                major: 'Computer Science',
+                graduationDate: 'May 2019',
+                gpa: '3.6'
+            },
+            {
+                degree: 'Bachelor of Science',
+                major: 'Computer Science',
+                graduationDate: 'May 2017',
+                gpa: '3.8'
+            }
+        ]
+    },
+    {
+        name: 'University of Nebraska at Lincoln',
+        location: 'Lincoln, NE',
+        degrees: [
+            {
+                degree: 'Bachelor of Science',
+                major: 'Math and Actuary Science',
+                graduationDate: 'May 2009',
+                gpa: '3.8'
+            }
+        ]
+    },
+];
--- src/data/work.js
+++ src/data/work.js
@@ -28,7 +28,7 @@ export const work = [
             'Support a Java based backend system.',
             'Develop a mapping prototype in using Angular and Leaflet libraries.',
             'Support the SEMS III contract for helping U.S. Air Force Weather (AFW).',
-            'Develop weather data ingestion and visualization products for the AFW-WEBS Website using Visual Weather.',  
+            'Develop weather data ingestion and visualization products for the AFW-WEBS Website using Visual Weather.',
             'Work with Open Geospatial Consortium (OGC) Standards for web services.',
             'Replace Java distributed caching platform, reducing vulnerability findings.',
             'Help develop full scale restructure of backend source code repositories.',
