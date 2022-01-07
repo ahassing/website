9cf8ba402d217236012134f637ec3b4cf9058ea8
Andrew Hassing <hassiar@huskers.unl.edu>
2022-01-06 22:53:07 -0600

    some more small adds

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import AboutMe from './pages/AboutMe';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import Footer from './Footer';
+import NavigationBar from './NavigationBar';
 
 const App = () =>  {
   return (
     <div className="ui container">
       <h1 className="ui center aligned header">Andrew Hassing</h1>
+      <NavigationBar />
       <AboutMe />
       <Blog />
       <Contact />
       <Resume />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            Created by Andrew Hassing, 2022
+        </div>
+    );
+}
+
+export default Footer;
