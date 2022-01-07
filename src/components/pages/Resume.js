a4a9c5eadf300585092129d09a550e8b70ee8d6c
Andrew Hassing <hassiar@huskers.unl.edu>
2022-01-06 21:08:22 -0600

    building out the resume and formatting

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { work } from "../../data/work";
+
+const renderPositions = (position, poindex) => {
+    return (
+        <div className="item" key={`position${poindex}`}>
+            <div>
+                {position.title}
+            </div>
+            <div>
+                {position.startDate} - {position.endDate}
+            </div>
+            <div className="ui bulleted list">
+                {position.duties.map( (duty, duindex) =>{
+                    return (
+                        <div className="item" key={`duty${duindex}`}>
+                            {duty}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+const renderResume = (data) => {
+    return (
+        <div className="ui list">
+            {data.map((company, coindex) => {
+                return (
+                    <div className="item" key={`company${coindex}`} >
+                        <div className="header">
+                            {company.name}
+                        </div>
+                        <div className="ui list">
+                            {company.positions.map((position, poindex) => renderPositions(position, poindex))}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
 
 const Resume = () => {
     return (
         <div>
-            Resume
+            {renderResume(work)}
         </div>
     )
 };
--- src/data/work.js
+++ src/data/work.js
@@ -1,21 +1,92 @@
-const work = {
-    companies: [
-        {
-            company: '',
-            positions: [
-                {
-                    position:'',
-                    startDate: '',
-                    endDate: '',
-                    location: '',
-                    duties: [
-                        '',
-                        '',
-                    ]
-                }
-            ],
-        }
-    ]
-};
-
-export default work;
+export const work = [
+    {
+        name: 'Spreetail, Inc.',
+        website: '',
+        positions: [
+            {
+                title: 'Software Engineer II',
+                startDate: 'May 2021',
+                endDate: 'Current',
+                location: 'Omaha, NE',
+                duties: [
+                    'Member of the Supply Chain - Operations software squad',
+                    'test',
+                ]
+            }
+        ],
+    },
+    {
+        name: 'Northrop Grumman',
+        website: '',
+        positions: [
+            {
+                title: 'Engineer Software',
+                startDate: 'May 2020',
+                endDate: 'April 2021',
+                location: 'Bellevue, NE',
+                duties: [
+                    'test',
+                    'test',
+                ]
+            },
+            {
+                title: 'Associate Engineer Software',
+                startDate: 'June 2017',
+                endDate: 'May 2020',
+                location: 'Bellevue, NE',
+                duties: [
+                    'test',
+                    'test',
+                ]
+            }
+        ],
+    },
+    {
+        name: 'Tenaska, Inc.',
+        website: '',
+        positions: [
+            {
+                title: 'C#/.Net Development Intern',
+                startDate: 'January 2016',
+                endDate: 'May 2017',
+                location: 'Omaha, NE',
+                duties: [
+                    'Member of the Supply Chain - Operations squad',
+                    'test',
+                ]
+            }
+        ],
+    },
+    {
+        name: 'Resource Insurance Consultants',
+        website: '',
+        positions: [
+            {
+                title: 'Life Insruance Analyst',
+                startDate: 'August 2012',
+                endDate: 'January 2016',
+                location: 'Omaha, NE',
+                duties: [
+                    'Member of the Supply Chain - Operations squad',
+                    'test',
+                ]
+            }
+        ],
+    },
+    {
+        name: 'Milliman, Inc.',
+        website: '',
+        positions: [
+            {
+                title: 'Actuarial Assistant',
+                startDate: 'October 2009',
+                endDate: 'May 2012',
+                location: 'Omaha, NE',
+                duties: [
+                    'Member of the Supply Chain - Operations squad',
+                    'test',
+                ]
+            }
+        ],
+    },
+];
